e1a890afd8780f83896a8d51f8bf6e98c3e63e88
carav008 <carav008@morris.umn.edu>
2017-09-07 22:11:14 -0500

    Add comments to explain our code

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -4,11 +4,22 @@ dir=$1
 
 
 #Create neccessary data for pie chart
+#We changed to specified scratch directory
 cd $dir
+
+#Searches and displays content of falied_login_data.text
+#Then using awk to print the fourth column (usersNames) and we get a count for each unique user name and output that to .txt
 grep -rh --include="failed_login_data.txt" . | awk '{print $4}' | sort | uniq -c | sort -nr | awk '{print "data.addRow([\x27"$2"\x27, " $1"]);"}' > temp.txt
 
+#We move the text file one directory back
 mv temp.txt ..
+
+#And go one directory back
 cd ..
+
+#Run wrap contents to create html file
 sh bin/./wrap_contents.sh temp.txt html_components/username_dist username_dist.html
 
+
+#Finally move html file  to the specified directory 
 mv username_dist.html $dir
--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-
+#Change to specified scratch dir 
 dir=$1
-
 cd ..
 cd $dir 
+
+#list all content of files in directories
+#Using awk we check columns then print specfied columns 
 grep -rh . | awk '{
 if (($6=="Failed")&&($10 == "user")){
 		{print ($1, $2, substr($3,0,2),$11,$13);}
@@ -13,4 +15,4 @@ else if(($6 == "Failed")){
 	       	{print ($1, $2, substr($3,0,2),$9,$11);}
 	}
 	}' | cat > failed_login_data.txt
-
+	#At the end we save the specified columns into failed_login_data.txt
