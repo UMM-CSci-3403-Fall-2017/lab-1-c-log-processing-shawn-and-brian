6b8f0fc518ca6ba83d8ee1f3ef1814d1cb4398b2
carav008 <carav008@morris.umn.edu>
2017-09-08 09:12:51 -0500

    Make test pass by adding reference to home

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+home=$(pwd)
 dir=$1
 
 
@@ -9,17 +10,19 @@ cd $dir
 
 #Searches and displays content of falied_login_data.text
 #Then using awk to print the fourth column (usersNames) and we get a count for each unique user name and output that to .txt
-grep -rh --include="failed_login_data.txt" . | awk '{print $4}' | sort | uniq -c | sort -nr | awk '{print "data.addRow([\x27"$2"\x27, " $1"]);"}' > temp.txt
+grep -rh --include="failed_login_data.txt" . | awk '{print $4}' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, " $1"]);"}' > temp.txt
 
 #We move the text file one directory back
-mv temp.txt ..
+mv temp.txt $home
 
 #And go one directory back
-cd ..
+cd $home
 
 #Run wrap contents to create html file
-sh bin/./wrap_contents.sh temp.txt html_components/username_dist username_dist.html
+sh ./bin/wrap_contents.sh temp.txt html_components/username_dist username_dist.html
 
 
 #Finally move html file  to the specified directory 
 mv username_dist.html $dir
+
+rm temp.txt
