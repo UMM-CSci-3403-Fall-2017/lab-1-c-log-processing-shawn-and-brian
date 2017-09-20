a95d3d73796abe822eb5959aaf4b25c54dab4a7b
carav008 <carav008@morris.umn.edu>
2017-09-19 20:17:23 -0500

    Make process_log.sh test pass

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-home=$(pwd)
 
-echo $home
+home=$(pwd)
 mkdir tmpScratch
 
+#This loop will loop over all command line args and will untar secure files into tmpScratch
 for var in "$@" 
 do 
+	#Gets the name of the *_secure.tgz and the 2nd var removes the .tgz
+	var1="${var##*/}"
+	var2="${var1%.*}"
+	mkdir tmpScratch/$var2
 
-	mkdir "${var%.*}"
-	tar -xf $var -C "${var%.*}"
-	mv "${var%.*}" tmpScratch
+	#Untar the files into specified directory
+	tar -xf $var -C tmpScratch/$var2
 
-	#call process_client
-	sh ./bin/process_client_logs.sh tmpScratch/"${var%.*}"
+	
+	#call process_client_logs.sh to create failed_login_data.txt
+	sh ./bin/process_client_logs.sh tmpScratch/$var2
 	
 done
 
-mv tmpScratch log_files
+#Then we will call helper scripts to create neccessary files
+sh ./bin/create_username_dist.sh tmpScratch
+sh ./bin/create_hours_dist.sh tmpScratch
+sh ./bin/create_country_dist.sh tmpScratch
+sh ./bin/assemble_report.sh tmpScratch
 
-sh ./bin/create_username_dist.sh log_files/tmpScratch
-sh ./bin/create_hours_dist.sh log_files/tmpScratch
-sh ./bin/create_country_dist.sh log_files/tmpScratch
+#Finally we will move the file we want to our top directory and cleanup
+mv tmpScratch/failed_login_summary.html $home
+rm -rf tmpScratch
 
-sh ./bin/assemble_report.sh log_files/tmpScratch
-
-cd log_files/tmpScratch
-mv failed_login_summary.html $home
