6e365addfedfdbbb90972dd2ac8bc604ac0247de
carav008 <carav008@morris.umn.edu>
2017-09-19 20:12:50 -0500

    Add extra awk condition to make process_logs.sh test pass

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-
+home=$(pwd)
 #Change to specified scratch dir 
 dir=$1
-cd ..
-cd $dir 
+cd $dir
 
 #list all content of files in directories
-#Using awk we check columns then print specfied columns 
-grep -rh . | awk '{
-if (($6=="Failed")&&($10 == "user")){
-		{print ($1, $2, substr($3,0,2),$11,$13);}
+#Using awk we check columns then print specfied columns
+#At the end we save the specified columns into failed_login_data.txt
+
+grep -rh . | awk  '/Failed password for/{
+if ($10 == "user" && $9=="invalid"){
+		print ($1, $2, substr($3,0,2),$11,$13);
 	}
-else if(($6 == "Failed")){
-	       	{print ($1, $2, substr($3,0,2),$9,$11);}
+else {
+	       	print ($1, $2, substr($3,0,2),$9,$11);
 	}
-	}' | cat > failed_login_data.txt
-	#At the end we save the specified columns into failed_login_data.txt
+	}'> failed_login_data.txt
+
